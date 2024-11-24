a7642135550a7e134544acf0df7c303153f2c06d
Saniya452 <ahmedsania198@gmail.com>
2024-11-24 08:32:02 -0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [randomBackground, setRandomBackground] = useState("#ffffff");
+
+  const generateColor = () => {
+    const hexRange = "0123456789ABCDEF"
+    let color = "#"
+
+    for (let i = 0; i < 6; i++) {
+      color += hexRange[Math.floor(Math.random() * 16)]
+      
+    }
+    setRandomBackground(color)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <StatusBar backgroundColor={randomBackground}/>
+    <View style={[styles.container, {backgroundColor: randomBackground}] }>
+      <TouchableOpacity onPress={generateColor}>
+          <Text style={styles.txt}>Your Fav Color</Text>
+        <View style={styles.actionBtn}>
+          <Text style={styles.actionBtnTxt}>Press me</Text>
+        </View>
+      </TouchableOpacity>
+      
     </View>
-  );
+    </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container:{
+    flex:1,
+    alignItems: "center",
+    justifyContent: "flex-end", 
+    paddingVertical: 40
+   },
+   txt:{
+    flex:1,
+    textAlign: 'center',
+    justifyContent:'center',
+    fontSize: 32,
+    fontWeight:'600'
+   },
+  actionBtn: {
+    borderRadius: 12,
+    backgroundColor: "#000000",
+    paddingVertical: 10,
+    paddingHorizontal: 40
   },
+  actionBtnTxt: {
+    fontSize: 24,
+    color: "#FFFFFF",
+    textTransform: "uppercase"
+  }
 });
+
+export default App;
